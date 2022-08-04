45e33fc59cd79917d147060c2276a9041d90e1e6
rktdnjs <rktdnjs@naver.com>
2022-08-04 17:42:45 +0900

    장르 및 이미지 추가

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
-  // 로딩을 위한 것 & 코인 리스트를 위한 것
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
-    },[])
+    getMovies();
+  },[]);
+  console.log(movies);
 
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `${coins.length}`}</h1>
-      {loading ? <strong>Loading....</strong> : 
-      <select>
-      {coins.map((coin) => 
-      <option>
-        {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}USD
-      </option>)}
-    </select>
-    }
+    <div> 
+      {loading ? <h1>로딩중...</h1> : movies.map((movie) => 
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image}/>
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map((g) => 
+            <li key={g}>{g}</li>)}
+        </ul>
+        </div>)}
     </div>
   )
 }
 
-{/* <ul> */}
-{/* {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}USD</li>)} */}
-{/* </ul> */}
 export default App; 
